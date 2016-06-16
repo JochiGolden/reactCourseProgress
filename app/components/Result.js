e04b76379dfd91b00ad420a4a95bdb1e10198122
JochiGolden <jacksonstruthers@gmail.com>
2016-06-16 10:24:10 +0200

    Finish UI for battle

--- app/components/Result.js
+++ app/components/Result.js
@@ -1,13 +1,56 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
+var Link = require('react-router').Link;
+var styles = require('../styles');
+var UserDetails = require('./UserDetails');
+var UserDetailsWrapper = require('./UserDetailsWrapper');
+var MainContentWrapper = require('./MainContentWrapper');
 
-function puke (obj) {
-  return <pre>{JSON.stringify(obj, 2, ' ')}</pre>
+function StartOverBtn () {
+  return (
+    <div className="col-sm-12" style={styles.space}>
+      <Link to="/playerOne">
+        <button type="button" className="btn btn-lg btn-danger">Start Over</button>
+      </Link>
+    </div>
+  );
 }
 
 function Result (props) {
+  
+  if (props.isLoading === true) {
+    return (
+      <MainContentWrapper>LOADING!!</MainContentWrapper>
+    );
+  }
+  
+  if (props.scores[0] === props.scores[1]) {
+    return (
+      <MainContentWrapper>
+        <h1>Tie!</h1>
+        <StartOverBtn />
+      </MainContentWrapper>
+    );
+  }
+  
+  var winningIndex = props.scores[0] > props.scores[1] ? 0 : 1;
+  var losingIndex = winningIndex === 0 ? 1 : 0;
+  
   return (
-    <div> Results: {props.scores[0]} to {props.scores[1]}</div>
+    <MainContentWrapper>
+      <div>
+        <h1>Results</h1>
+        <div className="col-sm-8 col-sm-offset-2">
+          <UserDetailsWrapper header="Winner">
+            <UserDetails score={props.scores[winningIndex]} info={props.playersInfo[winningIndex]} />
+          </UserDetailsWrapper>
+          <UserDetailsWrapper header="Loser">
+            <UserDetails score={props.scores[losingIndex]} info={props.playersInfo[losingIndex]} />
+          </UserDetailsWrapper>
+        </div>
+      </div>
+      <StartOverBtn />
+    </MainContentWrapper>
   );
 }
 
